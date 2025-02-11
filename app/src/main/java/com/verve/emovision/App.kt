a9fa82da8b1f22e3f49ec207df438ff990387014
GigihDC <112244673+GigihDC@users.noreply.github.com>
2025-02-11 11:27:50 +0700

    Initial commit

---
+++ app/src/main/java/com/verve/emovision/App.kt
@@ -0,0 +1,18 @@
+package com.verve.emovision
+
+import android.app.Application
+import com.verve.emovision.di.AppModules
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.GlobalContext.startKoin
+
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidLogger()
+            androidContext(this@App)
+            modules(AppModules.modules)
+        }
+    }
+}
